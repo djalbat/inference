eb40ace310fbc3b3d033ca21d4e86648c6670bb4
James Smith <james.smith@djalbat.com>
2017-03-21 12:02:19 +0000

    Simplified the build.

---
+++ es6/examples.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  reduxApp: require('./examples/reduxApp'),
+  inferenceApp: require('./examples/inferenceApp')
+};
+
--- examples/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  reduxApp: require('../lib/examples/reduxApp'),
-  inferenceApp: require('../lib/examples/inferenceApp')
-};
-
